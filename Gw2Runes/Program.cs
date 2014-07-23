6efb9251f7f5202a94606caed4a9eb5d8b8efd63
arazect <za3a24@yandex.ru>
2014-07-23 19:38:36 +0700

    AndaForce.dll now is embedded resource

--- Gw2Runes/Program.cs
+++ Gw2Runes/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Gw2Runes
 {
-    static class Program
+    internal static class Program
     {
+        const string Resource = "Gw2Runes.Resources.Libraries.AndaForce.dll";
+
         /// <summary>
-        /// Главная точка входа для приложения.
+        ///     Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            using (Stream stm = Assembly.GetExecutingAssembly().GetManifestResourceStream(Resource))
+            {
+                var ba = new byte[(int) stm.Length];
+                stm.Read(ba, 0, (int) stm.Length);
+
+                return Assembly.Load(ba);
+            }
         }
     }
 }
