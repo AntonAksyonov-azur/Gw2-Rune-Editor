8d1278dfa937be6466e62519909bfd4c2dc30f9e
arazect <za3a24@yandex.ru>
2014-07-24 16:44:46 +0700

    Added new components: ColorSelectionComboBox and ImageComboBox

---
+++ Gw2Runes/Components/ColorSelectionComboBox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gw2Runes.Components
+{
+    public class ColorSelectionComboBox : ComboBox
+    {
+        public ColorSelectionComboBox()
+        {
+            DrawMode = DrawMode.OwnerDrawFixed;
+            DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            if (e.Index >= 0 && e.Index < Items.Count)
+            {
+                var item = (ColorSelectionComboBoxItem) Items[e.Index];
+
+                e.Graphics.FillRectangle(new SolidBrush(item.Color), e.Bounds.Left, e.Bounds.Top, ItemHeight, ItemHeight);
+                e.Graphics.DrawString(item.Value, e.Font, new SolidBrush(e.ForeColor), e.Bounds.Left + ItemHeight, e.Bounds.Top + 2);
+            }
+
+            base.OnDrawItem(e);
+        }
+    }
+
+    public sealed class ColorSelectionComboBoxItem
+    {
+        public String Value;
+        public Color Color;
+
+        public ColorSelectionComboBoxItem(String val, Color color)
+        {
+            Value = val;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
---
+++ Gw2Runes/Components/ImageComboBox.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Gw2Runes.Components
+{
+    public class ImageComboBox : ComboBox
+    {
+        public ImageList ImageList { get; set; }
+
+        public ImageComboBox()
+        {
+            DrawMode = DrawMode.OwnerDrawFixed;
+            DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        public void ApplyImageList(ImageList list)
+        {
+            if (ImageList != null)
+            {
+                var result = ImageList.Images.Keys
+                    .OfType<String>()
+                    .OrderBy(a => a);
+
+                foreach (var imageKey in result)
+                {
+                    Items.Add(new ImageComboBoxItem(imageKey, ImageList.Images[imageKey]));
+                }
+
+                if (Items.Count > 0)
+                {
+                    SelectedItem = Items[0];
+                }
+            }
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            if (e.Index >= 0 && e.Index < Items.Count)
+            {
+                var item = (ImageComboBoxItem) Items[e.Index];
+
+                e.Graphics.DrawImage(item.Image, e.Bounds.Left, e.Bounds.Top);
+                e.Graphics.DrawString(item.Value, e.Font, new SolidBrush(e.ForeColor), e.Bounds.Left + item.Image.Width,
+                    e.Bounds.Top + 2);
+            }
+
+            base.OnDrawItem(e);
+        }
+    }
+
+    public sealed class ImageComboBoxItem
+    {
+        public String Value;
+        public Image Image;
+
+        public ImageComboBoxItem(String val, Image image)
+        {
+            Value = val;
+            Image = image;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
